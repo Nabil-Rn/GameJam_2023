2c2b0e1bb3c66ffe688bb9c9aa390bd7fe6653c5
Nabil-Rn <nabilr619@gmail.com>
2023-01-14 17:02:42 -0500

    BUTTONS

---
+++ GameJam2023/Fear_selection.java
@@ -0,0 +1,20 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Fear_selection extends World
+{
+
+    /**
+     * Constructor for objects of class Fear_selection.
+     */
+    public Fear_selection()
+    {
+        super(600, 400, 1);
+        
+    }
+}
