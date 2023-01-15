fc7cd94b27acc5e23977e175b8e35def3576795f
Nabil-Rn <nabilr619@gmail.com>
2023-01-15 11:15:05 -0500

    stuff
    boss
    player

---
+++ GameJam2023/Boss.java
@@ -0,0 +1,57 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Boss here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Boss extends Actor
+{
+    /**
+     * Act - do whatever the Boss wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        attack();
+        damage();
+        transitionToGameOver();
+    }
+    
+    public void attack()
+    {
+        int randNb = Greenfoot.getRandomNumber(100);
+        if (Greenfoot.getRandomNumber(100) < 3)
+        {
+            if (randNb == 0) 
+            {
+                //
+            }
+            if (randNb == 1) 
+            {
+                //
+            }
+            if (randNb == 2) 
+            {
+                //
+            }
+
+        }
+    }
+     public void damge()
+    {
+        if(){
+            
+        }
+    }
+    
+    public void transitionToGameOver()
+    {
+        World currentLevel = getWorld();
+        currentLevel.stopped();
+        //World gameOverWorld =  new GameOverWorld();
+        //gameOverWorld.started();
+        //Greenfoot.setWorld(gameOverWorld);
+    }
+}
---
+++ GameJam2023/LifeParameter.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class LifeParameter here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LifeParameter extends Actor
+{
+    private GreenfootImage heart1;
+    private GreenfootImage heart2;
+    private GreenfootImage heart3;
+    
+    Boss boss = (Boss) new Boss();
+    
+    
+    public LifeParameter() {
+        
+    }
+    /**
+     * Act - do whatever the LifeParameter wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        animateLifeParameter();
+    }
+    public void PlayerHealth() {
+                if () {
+            
+        }
+    }
+}
